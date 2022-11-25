27d897f9d2ac654c92a7acc0046fa1396ed7a8a3
sumitkumar22m <118610704+sumitkumar22m@users.noreply.github.com>
2022-11-25 18:13:43 +0530

    Add files via upload

---
+++ #14 Write a C Program to Calculate the Perimeter of a Square/Write a C Program to Calculate the Perimeter of a Square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,perimeter;
+    printf("\t\tFind the perimeter of the Square\n");
+    printf("Enter the value of a (side of a square)\n");
+    scanf("%d",&a);
+    perimeter=4*a;
+    printf("Perimeter of a Square = %d Unit",perimeter);
+    getch();
+}
